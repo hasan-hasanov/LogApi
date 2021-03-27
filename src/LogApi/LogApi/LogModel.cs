ceb767c21e117f1c568988fa45d2b3ab2ede9018
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-27 19:46:59 +0200

    primitive implmentation for log information

---
+++ src/LogApi/LogApi/LogModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LogApi
+{
+    public class LogModel
+    {
+        public LogModel(
+            string path,
+            string queryString,
+            Dictionary<string, string> headers,
+            Dictionary<string, string> cookies)
+        {
+            Path = path;
+            QueryString = queryString;
+            Headers = headers;
+            Cookies = cookies;
+        }
+
+        public string Path { get; }
+
+        public string QueryString { get; }
+
+        public Dictionary<string, string> Headers { get; }
+
+        public Dictionary<string, string> Cookies { get; }
+    }
+}
--- src/LogApi/LogApi/Startup.cs
+++ src/LogApi/LogApi/Startup.cs
@@ -1,19 +1,66 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace LogApi
 {
     public class Startup
     {
+        private readonly ConcurrentDictionary<string, IList<LogModel>> _clientLogs;
+
+        public Startup()
+        {
+            _clientLogs = new ConcurrentDictionary<string, IList<LogModel>>();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All,
+                RequireHeaderSymmetry = false,
+                ForwardLimit = null,
+                KnownNetworks = { new IPNetwork(IPAddress.Parse("::ffff:172.17.0.1"), 104) }
+            });
+
+            app.Run(async context =>
+            {
+                // TODO: Ignore browser favicon requests.
+
+                string ipAddress = context.Connection.RemoteIpAddress.ToString();
+                _clientLogs.AddOrUpdate(ipAddress, new List<LogModel>(), (key, value) =>
+                {
+                    if (value == null)
+                    {
+                        value = new List<LogModel>();
+                    }
+
+                    value.Add(new LogModel(
+                        context.Request.Path,
+                        context.Request.QueryString.ToString(),
+                        context.Request.Headers.ToDictionary(k => k.Key, v => string.Join(", ", v.Value)),
+                        context.Request.Cookies.ToDictionary(k => k.Key, v => v.Value)));
+                    return value;
+                });
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(_clientLogs[ipAddress]));
+            });
         }
     }
 }
