f56ce9957041a914911b79f2de377fe115e95c17
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-29 23:31:40 +0300

    Return ws message for all listeners on incomming requests

--- src/LogApi/LogApi/Startup.cs
+++ src/LogApi/LogApi/Startup.cs
@@ -18,11 +18,13 @@ namespace LogApi
 {
     public class Startup
     {
-        private readonly ConcurrentDictionary<string, IList<LogModel>> _clientLogs;
+        private readonly ConcurrentDictionary<string, LogModel> _clientLogs;
+        private readonly IList<WebSocket> _webSockets;
 
         public Startup()
         {
-            _clientLogs = new ConcurrentDictionary<string, IList<LogModel>>();
+            _clientLogs = new ConcurrentDictionary<string, LogModel>();
+            _webSockets = new List<WebSocket>();
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -52,6 +54,7 @@ namespace LogApi
                 {
                     using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
                     {
+                        _webSockets.Add(webSocket);
                         CancellationTokenSource cts = new(TimeSpan.FromMinutes(10));
 
                         byte[] message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(_clientLogs));
@@ -62,34 +65,48 @@ namespace LogApi
                             endOfMessage: true,
                             cts.Token);
 
-                        while (!cts.IsCancellationRequested)
+                        try
                         {
-                            await Task.Delay(TimeSpan.FromMinutes(5));
+                            while (!cts.IsCancellationRequested)
+                            {
+                                await Task.Delay(TimeSpan.FromSeconds(1), cts.Token);
+                            }
+                        }
+                        catch
+                        {
+                            _webSockets.Remove(webSocket);
                         }
                     }
                 }
                 else
                 {
                     // TODO: Ignore browser favicon requests.
-                    _clientLogs.AddOrUpdate(Guid.NewGuid().ToString(), new List<LogModel>(), (key, value) =>
-                    {
-                        if (value == null)
-                        {
-                            value = new List<LogModel>();
-                        }
-
-                        if (value.Count > 100)
-                        {
-                            value.RemoveAt(0);
-                        }
-
-                        value.Add(new LogModel(
+                    var logModel = new LogModel(
                             context.Request.Path,
                             context.Request.QueryString.ToString(),
                             context.Request.Headers.ToDictionary(k => k.Key, v => string.Join(", ", v.Value)),
-                            context.Request.Cookies.ToDictionary(k => k.Key, v => v.Value)));
-                        return value;
+                            context.Request.Cookies.ToDictionary(k => k.Key, v => v.Value));
+
+                    _clientLogs.AddOrUpdate(Guid.NewGuid().ToString(), logModel, (key, value) => logModel);
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    Task.Run(async () =>
+                    {
+                        foreach (var webSocket in _webSockets)
+                        {
+                            if (webSocket.State == WebSocketState.Open)
+                            {
+                                byte[] message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(logModel));
+
+                                await webSocket.SendAsync(
+                                    new ArraySegment<byte>(message, 0, message.Length),
+                                    WebSocketMessageType.Text,
+                                    endOfMessage: true,
+                                    CancellationToken.None);
+                            }
+                        }
                     });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
                     await context.Response.WriteAsync("Success!");
                 }
