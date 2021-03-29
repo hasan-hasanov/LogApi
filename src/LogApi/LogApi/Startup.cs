36147ecbd1423955fbccb3ea86ae6cf55158c0c5
Hasan Hasanov <hasan.mehmedov@gmail.com>
2021-03-29 22:22:30 +0300

    Add basic web socket support

--- src/LogApi/LogApi/Startup.cs
+++ src/LogApi/LogApi/Startup.cs
@@ -3,11 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.WebSockets;
+using System.Text;
 using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LogApi
 {
@@ -39,33 +44,56 @@ namespace LogApi
             });
 
             app.UseStaticFiles();
+            app.UseWebSockets();
 
             app.Run(async context =>
             {
-                // TODO: Ignore browser favicon requests.
-
                 string ipAddress = context.Connection.RemoteIpAddress.ToString();
-                _clientLogs.AddOrUpdate(ipAddress, new List<LogModel>(), (key, value) =>
+                if (context.WebSockets.IsWebSocketRequest)
                 {
-                    if (value == null)
+                    using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
                     {
-                        value = new List<LogModel>();
-                    }
+                        CancellationTokenSource cts = new CancellationTokenSource(TimeSpan.FromMinutes(10));
 
-                    if (value.Count > 100)
+                        while (!cts.IsCancellationRequested)
+                        {
+                            byte[] message = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(_clientLogs[ipAddress]));
+
+                            await webSocket.SendAsync(
+                                new ArraySegment<byte>(message, 0, message.Length),
+                                WebSocketMessageType.Text,
+                                endOfMessage: true,
+                                cts.Token);
+
+                            await Task.Delay(TimeSpan.FromMinutes(5));
+                        }
+                    }
+                }
+                else
+                {
+                    // TODO: Ignore browser favicon requests.
+                    _clientLogs.AddOrUpdate(ipAddress, new List<LogModel>(), (key, value) =>
                     {
-                        value.RemoveAt(0);
-                    }
+                        if (value == null)
+                        {
+                            value = new List<LogModel>();
+                        }
 
-                    value.Add(new LogModel(
-                        context.Request.Path,
-                        context.Request.QueryString.ToString(),
-                        context.Request.Headers.ToDictionary(k => k.Key, v => string.Join(", ", v.Value)),
-                        context.Request.Cookies.ToDictionary(k => k.Key, v => v.Value)));
-                    return value;
-                });
+                        if (value.Count > 100)
+                        {
+                            value.RemoveAt(0);
+                        }
 
-                await context.Response.WriteAsync(JsonSerializer.Serialize(_clientLogs[ipAddress]));
+                        value.Add(new LogModel(
+                            context.Request.Path,
+                            context.Request.QueryString.ToString(),
+                            context.Request.Headers.ToDictionary(k => k.Key, v => string.Join(", ", v.Value)),
+                            context.Request.Cookies.ToDictionary(k => k.Key, v => v.Value)));
+                        return value;
+                    });
+
+                    await context.Response.WriteAsync("Success!");
+                }
             });
         }
     }
